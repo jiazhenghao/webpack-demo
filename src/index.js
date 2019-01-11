c28737bf806ab0350c4456aa799f473e32747c9b
Zhenghao Jia <cherie-xia@hotmail.com>
2019-01-11 22:10:06 +0800

    管理资源
    管理资源

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './death.jpg';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
-    console.log("111122333");
+    console.log("test2");
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     //- Lodash, currently included via a script, is required for this line to work
     //+ Lodash, now imported by this script
-    
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     //element.innerHTML = "hello webpack111";
+
+    element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
     return element;
 }
 
