12dfe69ac56e897e67d7eec79cd7a68a0ac3ebbb
Zhenghao Jia <cherie-xia@hotmail.com>
2019-01-12 11:49:23 +0800

    webpack-指南-开发
    webpack-指南-开发

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-
-    // entry: './src/index.js',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
-        // filename: 'bundle.js',
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
@@ -23,38 +25,4 @@ module.exports = {
         })
     ]
 
-    // module: {
-    //     rules: [{
-    //             test: /\.css$/,
-    //             use: [
-    //                 'style-loader',
-    //                 'css-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(png|svg|jpg|gif)$/,
-    //             use: [
-    //                 'file-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
-    //             use: [
-    //                 'file-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(csv|tsv)$/,
-    //             use: [
-    //                 'csv-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.xml$/,
-    //             use: [
-    //                 'xml-loader'
-    //             ]
-    //         }
-    //     ]
-    // }
 };
